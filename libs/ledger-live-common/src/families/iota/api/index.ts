d0de3c1b05d6957f58df05e8a1cc82d59ccf7eec
Oliver <41125296+Oli-art@users.noreply.github.com>
2022-11-12 12:48:25 -0700

    LDGR-21 Refactor txToOp()
    Refactor txToOp() to be on the api files and not the bridge

--- libs/ledger-live-common/src/families/iota/api/index.ts
+++ libs/ledger-live-common/src/families/iota/api/index.ts
@@ -2,11 +2,14 @@ import BigNumber from "bignumber.js";
 import { getEnv } from "../../../env";
 import network from "../../../network";
 import type { Operation } from "@ledgerhq/types-live";
-import { txToOp } from "../bridge/js";
-import { Block, OutputResponse, OutputsResponse } from "./types";
+import {
+  Block,
+  OutputResponse,
+  OutputsResponse,
+  TransactionPayload,
+} from "./types";
+import { Bech32 } from "@iota/crypto.js";
 
-const getIotaUrl = (route): string =>
-  `${getEnv("API_IOTA_NODE")}${route || ""}`;
 const getShimmerUrl = (route): string =>
   `${getEnv("API_SHIMMER_NODE")}${route || ""}`;
 
@@ -77,7 +80,9 @@ const fetchAllOutputs = async (address: string) => {
   return data as OutputsResponse;
 };
 
-export const fetchSingleOutput = async (outputId: string) => {
+export const fetchSingleOutput = async (
+  outputId: string
+): Promise<OutputResponse> => {
   const {
     data,
   }: {
@@ -100,7 +105,10 @@ export const getAccount = async (address: string): Promise<any> => {
   };
 };
 
-export const getOperations = async (id: string, address: string) => {
+export const getOperations = async (
+  id: string,
+  address: string
+): Promise<Operation[]> => {
   const operations: Operation[] = [];
   const { transactions, timestamps, transactionIds } =
     await fetchAllTransactions(address);
@@ -117,4 +125,100 @@ export const getOperations = async (id: string, address: string) => {
     }
   }
   return operations;
+};
+
+const txToOp = async (
+  transaction: Block,
+  id: string,
+  address: string,
+  timestamp: number,
+  transactionId: string
+): Promise<any> => {
+  const data = transaction ? transaction : null;
+  if (!data || !data.payload || data.payload?.type != 6) {
+    return null;
+  }
+
+  // define the outputs and inputs of the transaction / block
+  const payload = data.payload as TransactionPayload;
+  const essence = payload.essence;
+  const outputs = essence.outputs;
+  const inputs = essence.inputs;
+
+  const recipients: string[] = [];
+  const senders: string[] = [];
+  let value = 0;
+  let type: "IN" | "OUT" = "IN"; // default is IN. If the address is found in an input, it will be changed to "OUT"
+
+  // senders logic
+  for (let i = 0; i < inputs.length; i++) {
+    const transactionId = inputs[i].transactionId;
+    const outputIndex = "0" + inputs[i].transactionOutputIndex;
+    const senderOutput = (await fetchSingleOutput(transactionId + outputIndex))
+      .output;
+    const senderUnlockCondition: any = senderOutput.unlockConditions[0];
+    const senderPubKeyHash: any = senderUnlockCondition.address.pubKeyHash;
+    const senderUint8Array = Uint8Array.from(
+      senderPubKeyHash
+        .match(/.{1,2}/g) // magic
+        .map((byte: string) => parseInt(byte, 16))
+    );
+    // the address of the sender
+    const sender = Bech32.encode("smr", senderUint8Array);
+    senders.push(sender);
+    if (sender == address) type = "OUT";
+  }
+
+  // receivers logic
+  for (let o = 0; o < outputs.length; o++) {
+    if (outputCheck(outputs[o])) {
+      const recipientUnlockCondition: any = outputs[o].unlockConditions[0];
+      const recipientPubKeyHash: any =
+        recipientUnlockCondition.address.pubKeyHash;
+      const recipientUint8Array = Uint8Array.from(
+        recipientPubKeyHash
+          .match(/.{1,2}/g) // magic
+          .map((byte: string) => parseInt(byte, 16))
+      );
+      // the address of the recipient
+      const recipient = Bech32.encode("smr", recipientUint8Array);
+
+      // In case the transaction is incoming:
+      // add to the value all amount coming into the address.
+      // If the transaction is outgoing:
+      // add to the value all amount going to other addresses.
+      const amount: number = +outputs[o].amount;
+      if (type == "IN" && recipient == address) value += amount;
+      else if (type == "OUT" && recipient != address) value += amount; // otherwise, it means that it's a remainder and doesn't count into the value
+
+      recipients.push(recipient);
+    }
+  }
+
+  const op: Operation = {
+    id: `${transactionId}-${type}`,
+    hash: transactionId, // TODO: Pass the transaction id instead
+    type,
+    value: new BigNumber(value),
+    fee: new BigNumber(0),
+    blockHash: "",
+    blockHeight: 10, // so it's considered a confirmed transaction
+    senders,
+    recipients,
+    accountId: id,
+    date: new Date(timestamp * 1000),
+    extra: {},
+  };
+
+  return op;
+};
+
+const outputCheck = (output: any): boolean => {
+  if (
+    output.type == 3 && // it's a BasicOutput
+    output.unlockConditions.length == 1 && // no other unlockConditions
+    output.unlockConditions[0].type == 0 // it's an AddressUnlockCondition
+  ) {
+    return true;
+  } else return false;
 };
--- libs/ledger-live-common/src/families/iota/bridge/js.ts
+++ libs/ledger-live-common/src/families/iota/bridge/js.ts
@@ -1,5 +1,4 @@
-import { BigNumber } from "bignumber.js";
-import type { Account, AccountBridge, Operation } from "@ledgerhq/types-live";
+import type { Account, AccountBridge } from "@ledgerhq/types-live";
 import {
   createTransaction,
   updateTransaction,
@@ -10,98 +9,9 @@ import type { Transaction, TransactionStatus } from "../types";
 import { makeAccountBridgeReceive } from "../../../bridge/jsHelpers";
 import { CurrencyNotSupported } from "@ledgerhq/errors";
 import estimateMaxSpendable from "../js-estimateMaxSpendable";
-import { TransactionPayload, Block } from "../api/types"; //xddxd
 import { currencyBridge, sync } from "../js-synchronisation";
-import { fetchSingleOutput } from "../api";
-import { Bech32 } from "@iota/crypto.js";
 
 const receive = makeAccountBridgeReceive();
-
-export const txToOp = async (
-  transaction: Block,
-  id: string,
-  address: string,
-  timestamp: number,
-  transactionId: string
-): Promise<any> => {
-  const data = transaction ? transaction : null;
-  if (!data || !data.payload || data.payload?.type != 6) {
-    return null;
-  }
-
-  // define the outputs and inputs of the transaction / block
-  const payload = data.payload as TransactionPayload;
-  const essence = payload.essence;
-  const outputs = essence.outputs;
-  const inputs = essence.inputs;
-
-  const recipients: string[] = [];
-  const senders: string[] = [];
-  let value = 0;
-  let type: "IN" | "OUT" = "IN"; // default is IN. If the address is found in an input, it will be changed to "OUT"
-
-  // senders logic
-  for (let i = 0; i < inputs.length; i++) {
-    const transactionId = inputs[i].transactionId;
-    const outputIndex = "0" + inputs[i].transactionOutputIndex;
-    const senderOutput = (await fetchSingleOutput(transactionId + outputIndex))
-      .output;
-    const senderUnlockCondition: any = senderOutput.unlockConditions[0];
-    const senderPubKeyHash: any = senderUnlockCondition.address.pubKeyHash;
-    const senderUint8Array = Uint8Array.from(
-      senderPubKeyHash
-        .match(/.{1,2}/g) // magic
-        .map((byte: string) => parseInt(byte, 16))
-    );
-    // the address of the sender
-    const sender = Bech32.encode("smr", senderUint8Array);
-    senders.push(sender);
-    if (sender == address) type = "OUT";
-  }
-
-  // receivers logic
-  for (let o = 0; o < outputs.length; o++) {
-    if (outputCheck(outputs[o])) {
-      const recipientUnlockCondition: any = outputs[o].unlockConditions[0];
-      const recipientPubKeyHash: any =
-        recipientUnlockCondition.address.pubKeyHash;
-      const recipientUint8Array = Uint8Array.from(
-        recipientPubKeyHash
-          .match(/.{1,2}/g) // magic
-          .map((byte: string) => parseInt(byte, 16))
-      );
-      // the address of the recipient
-      const recipient = Bech32.encode("smr", recipientUint8Array);
-
-      // In case the transaction is incoming:
-      // add to the value all amount coming into the address.
-      // If the transaction is outgoing:
-      // add to the value all amount going to other addresses.
-      const amount: number = +outputs[o].amount;
-      if (type == "IN" && recipient == address) value += amount;
-      else if (type == "OUT" && recipient != address) value += amount; // otherwise, it means that it's a remainder and doesn't count into the value
-
-      recipients.push(recipient);
-    }
-  }
-
-  const op: Operation = {
-    id: `${transactionId}-${type}`,
-    hash: transactionId, // TODO: Pass the transaction id instead
-    type,
-    value: new BigNumber(value),
-    fee: new BigNumber(0),
-    blockHash: "",
-    blockHeight: 10, // so it's considered a confirmed transaction
-    senders,
-    recipients,
-    accountId: id,
-    date: new Date(timestamp * 1000),
-    extra: {},
-  };
-
-  return op;
-};
 
 const getTransactionStatus = (a: Account): Promise<TransactionStatus> =>
   Promise.reject(
@@ -109,16 +19,6 @@ const getTransactionStatus = (a: Account): Promise<TransactionStatus> =>
       currencyName: a.currency.name,
     })
   );
-
-const outputCheck = (output: any): boolean => {
-  if (
-    output.type == 3 && // it's a BasicOutput
-    output.unlockConditions.length == 1 && // no other unlockConditions
-    output.unlockConditions[0].type == 0 // it's an AddressUnlockCondition
-  ) {
-    return true;
-  } else return false;
-};
 
 const accountBridge: AccountBridge<Transaction> = {
   sync,
